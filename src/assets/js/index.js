8a7ec5fccfe2b43c06e3fd789eb2daa7667ada49
TheMativious <130515015+TheMativious@users.noreply.github.com>
2024-02-29 12:18:13 -0300

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -83,10 +83,10 @@ class Splash {
     }
 
     shutdown(text) {
-        this.setStatus(`${text}<br>Arrêt dans 5s`);
+        this.setStatus(`${text}<br>Error y cerrando launcher 5s`);
         let i = 4;
         setInterval(() => {
-            this.setStatus(`${text}<br>Arrêt dans ${i--}s`);
+            this.setStatus(`${text}<br>Error y cerrando launcher ${i--}s`);
             if (i < 0) ipcRenderer.send('update-window-close');
         }, 1000);
     }
